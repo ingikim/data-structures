02d2814a899b3bf02d50bbb054c542ee9b753c7d
ingikim <bcmet365@daum.net>
2015-07-01 16:40:31 -0700

    progress

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+var stackMethods = { 
+	storage:{},
+	length: 0};
+
+stackMethods.size = size;
+stackMethods.push = push;
+stackMethods.pop = pop;
+
+return stackMethods;
 };
 
-var stackMethods = {};
+var size = function(){
+	return length;
+};
 
+var push = function(value){
+	length++;
+	storage[length] = value;
+};
 
+var pop = function(){
+	if(length > 0){
+		length--;
+		return storage[length + 1];
+	}
+};
