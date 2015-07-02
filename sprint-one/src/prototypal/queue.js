3b5595b4b8cc258745204d69b8ca642851462252
ingikim <bcmet365@daum.net>
2015-07-01 20:22:14 -0700

    finished prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.length = 0;
+  obj.counter = 0;
+  obj.increase = 0;
+
+  return obj;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+	return this.length;
+}
 
+queueMethods.enqueue = function(value) {
+	this.length++;
+	this.increase++;
+	this[this.increase] = value;
+}
+
+queueMethods.dequeue = function() {
+	if(this.length > 0) {
+		this.length--;
+		this.counter++;
+		return this[this.counter];
+	}
+}
