7d86ed7546dabd5584ebb1e9f03668a4063d1a73
ingikim <bcmet365@daum.net>
2015-07-01 20:31:03 -0700

    completed pseudoclassical stack.js

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
 };
 
+Stack.prototype.size = function() {
+	return this.length;
+}
 
+Stack.prototype.push = function(value) {
+	this.length++;
+	this[this.length] = value;
+}
+
+Stack.prototype.pop = function() {
+	if(this.length > 0) {
+		this.length--;
+		return this[this.length + 1];
+	}
+}
