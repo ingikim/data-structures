bcd278cbe015a31151c059f3f49461e72c5c8dd5
ingikim <bcmet365@daum.net>
2015-07-02 14:19:59 -0700

    working on linkedlistspec.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,28 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var nodeContainer = {};
+  var keymaker = 0;
 
   list.addToTail = function(value){
+    var newtail = Node(value);
+    newtail.value = value;
+    list.tail = newtail;
+    keymaker++;
+    newtail.next = keymaker;
+    nodeContainer[keymaker - 1] = newtail;
+    if(!list.head) {
+      list.head = list.tail;
+    } 
   };
 
   list.removeHead = function(){
+    if(list.head === list.tail) {
+      return list.head.value;
+    } else {
+      list.head  = nodeContainer[list.head.next];
+      return nodeContainer[list.head.next - 1].value; 
+    }
   };
 
   list.contains = function(target){
