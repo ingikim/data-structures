799b225f940549d8cc377c39cff9d3a543056d18
ingikim <bcmet365@daum.net>
2015-07-03 16:12:19 -0700

    finished set.js graph.js mostly complete

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,58 @@
 
 
 var Graph = function(){
+	this.storage = [];
+	this.edges = [];
 };
 
 Graph.prototype.addNode = function(node){
+	this.storage.push(node);
 };
 
 Graph.prototype.contains = function(node){
+	var result = false;
+	_.each(this.storage, function(item) {
+		if(item === node) {
+			result = true;
+		}
+	});
+	return result;
 };
 
 Graph.prototype.removeNode = function(node){
+	var index = _.indexOf(this.storage, node);
+	if(index >= 0) {
+		this.storage.splice(index, 1);
+	}
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+	var result = false;
+	_.each(this.edges, function(item) {
+		if(item[0] === fromNode && item[1] === toNode) {
+			result = true;
+		}
+	});
+	return result;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	this.edges.push([fromNode, toNode], [toNode, fromNode]);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+	_.each(this.edges, function(item, index, array) {
+		if(item[0] === fromNode && item[1] === toNode) {
+			array.splice(index, 1);
+		}
+		if(item[0] === toNode && item[1] === fromNode) {
+			array.splice(index, 1);
+		}
+	});
 };
 
 Graph.prototype.forEachNode = function(cb){
+	
 };
 
 /*
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+	var index = _.indexOf(this._storage, item);
+	if(index !== -1) {
+		return true;
+	}
+	return false;
 };
 
 setPrototype.remove = function(item){
+	if(this.contains(item)) {
+		_.each(this._storage, function(value, index, array) {
+			if(value === item) {
+				array.splice(index, 1);
+			}
+		});
+	}
 };
 
 /*
